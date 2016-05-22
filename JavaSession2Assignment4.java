c2337b6906779070dfe206fb7ff94d92f0a90344
QuintenDavis <Quinten.R.Davis@gmail.com>
2016-05-22 16:41:59 -0400

    Add files via upload

---
+++ JavaSession2Assignment4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class JavaSession2Assignment4 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int month = s.nextInt();
+		switch (month)
+		{
+		    case 1 : System.out.println("31 Days");break;
+		    case 2: System.out.println("28 Days");break;
+		    case 3 : System.out.println("31 Days");break;
+		    case 4 : System.out.println("30 Days");break;
+		    case 5 : System.out.println("31 Days");break;
+		    case 6 : System.out.println("30 Days");break;
+		    case 7 : System.out.println("31 Days");break;
+		    case 8 : System.out.println("31 Days");break;
+		    case 9 : System.out.println("30 Days");break;
+		    case 10 : System.out.println("31 Days");break;
+		    case 11 : System.out.println("30 Days");break;
+		    case 12 : System.out.println("31 Days");break;
+		    
+		}
+	
+
+	}
+
+}
